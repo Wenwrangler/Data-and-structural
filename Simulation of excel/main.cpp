6065de3a4acc6c3354e0c77b26804909a2f6a00e
Wenwrangler <1752456324@qq.com>
2019-06-21 22:41:42 +0800

    画出基本表格

--- Simulation of excel/main.cpp
+++ Simulation of excel/main.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 int main() {
 	initgraph(1024, 768);
+	setbkcolor(YELLOW);
+	setfillcolor(YELLOW);
+	setlinecolor(BLACK);
+	int x = 100, y = 25;
+	for (int i = 1; i < 10000; i++) {
+		for (int j = 1; j < 1000; j++) {
+			fillrectangle(256*i-256, j*50-50, 256 * i, 50*j);
+			setfillcolor(YELLOW);
+			setlinecolor(BLACK);
+			settextcolor(RED);
+			char s[] = "Hello World";
+			outtextxy(x+(i*256-256), y+(j*50-50), s);
+			
+		}
+		
+	}
+	
 	_getch();
 	cout << "1";
+	return 0;
 }
